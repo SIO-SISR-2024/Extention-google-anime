fde71eec9b362bf24b225461443fc9dc18c1d881
Scorpionxpx <118664491+Scorpionxpx@users.noreply.github.com>
2024-04-24 15:00:34 +0200

    2eme test avec le nv mode de fonctionnement
    Changement de la détection de l'épisode

--- background.js
+++ background.js
@@ -5,15 +5,18 @@ chrome.action.onClicked.addListener(async (tab) => {
     // Récupère l'URL de l'onglet actif
     let currentUrl = activeTab.url;
   
-    // Recherche un nombre dans l'URL (par exemple, "episode=5")
-    let match = currentUrl.match(/episode=(\d+)/);
+    // Recherche un nombre dans l'URL (par exemple, "03_vostfr")
+    let match = currentUrl.match(/(\d+)_vostfr/);
     if (match) {
       // Incrémente le nombre d'épisode
       let episodeNumber = parseInt(match[1]);
       let nextEpisode = episodeNumber + 1;
   
+      // Formate le numéro de l'épisode pour qu'il corresponde à "03" dans l'URL
+      let formattedEpisode = nextEpisode.toString().padStart(2, "0");
+  
       // Remplace le nombre d'épisode dans l'URL avec le suivant
-      let newUrl = currentUrl.replace(match[0], "episode=" + nextEpisode);
+      let newUrl = currentUrl.replace(match[0], formattedEpisode + "_vostfr");
   
       // Ouvre un nouvel onglet avec l'URL modifiée
       chrome.tabs.create({ url: newUrl });
